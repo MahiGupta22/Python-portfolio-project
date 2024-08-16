1bcda25f736adbd0149493eb1e05d5ffd2c15c0d
MahiGupta22 <guptamahi0305@gmail.com>
2024-08-16 13:01:28 +0530

    Update day5.py

--- learning/day5.py
+++ learning/day5.py
@@ -24,11 +24,22 @@ for i in range(1,101):
 
 #Password Generator
 import random
+letters=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+numbers=["0","1","2","3","4","5","6","7","8","9"]
+symbols=['!',"@","#","$","%","&","(",")","*","?","+"]
 print("Welcome to the pyPassword Generator")
-letters=int(input("how many letters would you like in your password?\n"))
-symbols=int(input("how many symbols would you like in your password?\n"))
-numbers=int(input("how many numbers would you like in your password?\n"))
-password=""
-range(1,101)
-for char in range(1,letters+1):
-    password+=random.choice(letters)
+num_letters=int(input("how many letters would you like in your password?\n"))
+num_symbols=int(input("how many symbols would you like in your password?\n"))
+num_numbers=int(input("how many numbers would you like in your password?\n"))
+password=[]
+for char in range(0,num_letters):
+    password.append(random.choice(letters))
+for char in range(0,num_symbols):
+    password.append(random.choice(symbols))
+for char in range(0,num_numbers):
+    password.append(random.choice(numbers))
+random.shuffle(password)
+passwords=""
+for pw in password:
+    passwords+=pw
+print(passwords)
